1b1fed77f7385eb531114ec4a3120ed88bb0fda5
Murilo Rerison de Sousa Paz <murilokapaz@gmail.com>
2019-12-02 18:03:32 -0300

    first commit

---
+++ DLLImportaBancoDeDados/Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GO.ECM.ScriptDotNet.Atributos;
+using GO.ECM.ScriptDotNet.ProdutoEspecifico;
+using ConsoleApp1.Entities;
+using System.Data.SqlClient;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace DLLImportaBancoDeDados
+{
+    [ClasseScript]
+    public class Class1 : ScriptEcm
+    {
+        public Class1()
+        {
+
+        }
+        public Class1(Object obj)
+        {
+
+        }
+
+        [MetodoScript]
+        public void ImportaBanco()
+        {
+            string strConexao = "Server=osas;Initial Catalog=DB_CadastroFunc;user id=sa;pwd=x";
+            var conn = new SqlConnection(strConexao);
+            conn.Open();
+            var comm = new SqlCommand("select * from Cadastro", conn);
+            SqlDataReader dr = comm.ExecuteReader();
+
+            var funcionarios = new List<Funcionario>();
+            string[] valores = new string[7];
+            int count = 0;
+            while (dr.Read())
+            {
+                for (int i = 0; i < dr.FieldCount; i++)
+                {
+                    valores[count] = dr[i].ToString();
+                    if (count > 5)
+                    {
+                        funcionarios.Add(new Funcionario
+                        {
+                            Id = int.Parse(valores[0]),
+                            Matricula = valores[1],
+                            Graduacao = valores[2],
+                            NomeDeGuerra = valores[3],
+                            Nome = valores[4],
+                            Quantidade = int.Parse(valores[5]),
+                            Ativo = bool.Parse(valores[6])
+                        });
+                        count = 0;
+                    }
+                    else
+                    {
+                        count++;
+                    }
+                }
+
+            }
+
+            dr.Close();
+            conn.Close();
+            var json = new JavaScriptSerializer().Serialize(funcionarios);
+            base.SetCampoValor(8, json);
+        }
+
+    }
+}
---
+++ DLLImportaBancoDeDados/Entities/Funcionario.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace ConsoleApp1.Entities
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Matricula { get; set; }
+        public int Quantidade { get; set; }
+        public string Graduacao { get; set; }
+        public string NomeDeGuerra { get; set; }
+        public string Nome { get; set; }
+        public bool Ativo { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Id: {Id}");
+            sb.AppendLine($"Matrícula: {Matricula}");
+            sb.AppendLine($"Quantidade: {Quantidade}");
+            sb.AppendLine($"Graduação:{Graduacao}");
+            sb.AppendLine($"Nome de Guerra:{NomeDeGuerra}");
+            sb.AppendLine($"Nome:{Nome}");
+            sb.AppendLine($"Ativo? {Ativo}");
+
+            return sb.ToString();
+        }
+    }
+}
